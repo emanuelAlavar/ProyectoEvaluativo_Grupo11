7e0cabda687e2db8cf8a1dd909365acce97e224b
NoJsNoLife <fnayala.jr@gmail.com>
2022-05-29 18:20:25 -0300

    AGREGO ENLACES, CORRIJO LA LISTACANDIDATOS, FALTA CONTEO DE VOTOS, CORRIJO CONTROLADORES

--- ProyectoEvaluativo_Grupo11/src/main/java/ar/edu/unju/fi/controllers/CandidatoController.java
+++ ProyectoEvaluativo_Grupo11/src/main/java/ar/edu/unju/fi/controllers/CandidatoController.java
@@ -2,16 +2,17 @@ package ar.edu.unju.fi.controllers;
 
 import org.apache.commons.logging.LogFactory;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import ar.edu.unju.fi.models.Candidato;
@@ -25,9 +26,9 @@ public class CandidatoController {
 	ListaCandidatos listaCandidatos = new ListaCandidatos();
 	
 	@GetMapping("/candidato")
-	public ModelAndView getCandidatos(Model model) {
+	public ModelAndView getCandidatosPage(@ModelAttribute("candidato") Candidato candidato) {
 		ModelAndView mav = new ModelAndView("lista_candidatos");
-		mav.addObject("candidato", listaCandidatos.getCandidatos());
+		mav.addObject("candidatos", listaCandidatos.getCandidatos());
 		return mav;
 	}
 	
@@ -48,23 +49,15 @@ public class CandidatoController {
 		return mav;
 	}
 	
-	public List<Candidato> getCandidatos(){
-		List<Candidato> listaCandidatos = new ArrayList<>();
-		listaCandidatos.add(new Candidato(10,"Pablo Lescano","Masculino","Es un cantante y músico argentino, líder del grupo Damas Gratis"));
-		listaCandidatos.add(new Candidato(9,"Maluma","Masculino","Es un actor y cantante colombiano de pop, reguetón y trap latino"));
-		listaCandidatos.add(new Candidato(8,"Tini","Femenino","Es una actriz, cantante y compositora argentina"));
-		return listaCandidatos;
-	}
-	
-
-	@PostMapping("/viewCandidatos")
+	@GetMapping("/")
 	public String visualizarPaginaCandidatos(Model model) {	
-    	model.addAttribute("candidatos",this.getCandidatos());
+    	model.addAttribute("candidatos",listaCandidatos.getCandidatos());
     	LOGGER.info("REQUEST: /viewCandidatos - METHOD: visualizarPaginaCandidatos() - INFO: Se agregan al contexto de la aplicación la colección de posts");
-    	for(Candidato c:this.getCandidatos()) {
+    	for(Candidato c:listaCandidatos.getCandidatos()) {
     		LOGGER.info(c.toString());
     	}
     	LOGGER.info("REQUEST: /viewPosts - METHOD: visualizarPaginaPosts() - INFO: Se solicita la página post.html");
-    	return "candidatos";
+    	return "index";
 	}
+	
 }

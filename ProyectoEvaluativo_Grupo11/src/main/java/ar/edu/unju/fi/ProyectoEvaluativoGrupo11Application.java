5e43c3c185cbc541469b0d9dd56ffd35d2d043e4
emanuelAlavar <manusovich15@gmail.com>
2022-05-29 01:03:46 -0300

    Creacion del Proyecto Evaluativo

---
+++ ProyectoEvaluativo_Grupo11/src/main/java/ar/edu/unju/fi/ProyectoEvaluativoGrupo11Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEvaluativoGrupo11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEvaluativoGrupo11Application.class, args);
+	}
+
+}
